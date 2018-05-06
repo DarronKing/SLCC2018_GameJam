07a952021428e2b35c9240abc6b851ff76ec3619
Dayer King <ikingst7@gmail.com>
2018-05-05 20:15:30 -0600

    Basic Unity Project

---
+++ Assets/PlayerMovementNoRigid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementNoRigid : MonoBehaviour {
+
+	public float speed;
+
+    void Start ()
+    {
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+
+        transform.Translate((movement * Time.deltaTime) * speed);
+    }
+}
